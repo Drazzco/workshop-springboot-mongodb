8445dee58cfe60d4b612433bfa06610e2d6ccc6f
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2023-07-06 15:45:02 -0300

    FindById implementation

--- src/main/java/com/drazzco/workshopmongo/services/UserService.java
+++ src/main/java/com/drazzco/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.drazzco.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.drazzco.workshopmongo.entities.User;
 import com.drazzco.workshopmongo.repositories.UserRepository;
+import com.drazzco.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	public List<User> findAll()
 	{
 		return repository.findAll();
+	}
+	
+	public User findById(String id)
+	{
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
