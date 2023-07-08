bbfa273c8ee4abdc9013066d9d09d3566a40ad64
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2023-07-08 09:00:17 -0300

    Delete implementation

--- src/main/java/com/drazzco/workshopmongo/services/UserService.java
+++ src/main/java/com/drazzco/workshopmongo/services/UserService.java
@@ -33,6 +33,12 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	public void delete(String id)
+	{
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO dto)
 	{
 		return new User(dto.getId(), dto.getName(), dto.getEmail());
