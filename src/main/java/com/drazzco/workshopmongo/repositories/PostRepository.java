a7fa22497f035d5d6eac39b8aa8e2ccff282201a
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2023-07-08 23:02:04 -0300

    Simple search using query methods

--- src/main/java/com/drazzco/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/drazzco/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,12 @@
 package com.drazzco.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.drazzco.workshopmongo.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
