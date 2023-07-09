423476337a0011b923cb2bd5aec1dab6df2d32df
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2023-07-08 21:53:40 -0300

    Find Post by ID

---
+++ src/main/java/com/drazzco/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.drazzco.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.drazzco.workshopmongo.entities.Post;
+import com.drazzco.workshopmongo.repositories.PostRepository;
+import com.drazzco.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id)
+	{
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+}
